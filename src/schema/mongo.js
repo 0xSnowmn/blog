4aa99d5c3c3e2c5c69cfad95552b45519d308830
Khaled Nassar <knassar702@gmail.com>
2021-08-25 17:57:33 +0200

    add more mongodb schema

--- src/schema/mongo.js
+++ src/schema/mongo.js
@@ -23,4 +23,47 @@ const UsersSchema = new Schema({
     created: { type: Date, default: Date.now },
 });
 
-module.exports = model('Users',UsersSchema)
+const PostsSchema = new Schema ({
+    title:{
+        type: String,
+        min:2,
+        max:100,
+        required: true
+    },
+    content:{
+        type: String,
+        min:2,
+        max:600,
+        required: true
+    },
+    tags:{
+        type: Array
+    },
+    comments: [{
+         type: mongoose.Schema.Types.ObjectId,
+         ref: 'Comment'
+       }]
+})
+
+const commentSchema = new mongoose.Schema({
+     text: {
+          type: String,
+          trim: true,
+          required: true
+       },
+    date: {
+          type: Date,
+          default: Date.now
+       },
+    // each comment can only relates to one blog, so it's not in array
+    post: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Post'
+       }
+})
+
+
+
+module.exports.UsersSchema = model('Users',UsersSchema)
+module.exports.commentSchema = model('Comments',commentSchema)
+module.exports.PostsSchema = model('Posts',PostsSchema)
